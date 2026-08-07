b3c3dafb77a8bd26e5c90f630e93d525a96bc472
todo996 <kuquan55555@gmail.com>
2026-08-07 17:08:48 +0700

    fix(build): decouple union log controller from UI renderer

--- assets/scripts/union/UnionLogLogic.ts
+++ assets/scripts/union/UnionLogLogic.ts
@@ -1,103 +1,46 @@
-import { _decorator, Color, Component, Graphics, ScrollView, UITransform } from 'cc';
-const {ccclass, property} = _decorator;
-
-import UnionCommand from "./UnionCommand";
-import { Union } from "./UnionProxy";
-import { MapCityData } from "../map/MapCityProxy";
-import MapCommand from "../map/MapCommand";
-import { EventMgr } from '../utils/EventMgr';
+import { _decorator, Component, ScrollView } from 'cc';
 import { LogicEvent } from '../common/LogicEvent';
+import { MapCityData } from '../map/MapCityProxy';
+import MapCommand from '../map/MapCommand';
+import { EventMgr } from '../utils/EventMgr';
 import ListLogic from '../utils/ListLogic';
-import { createGameText, ensureChild, ensureTransform } from '../ui/components/GameSurface';
-import { GameTheme } from '../ui/theme/GameTheme';
+import UnionCommand from './UnionCommand';
+import { Union } from './UnionProxy';
+
+const { ccclass, property } = _decorator;
 
 @ccclass('UnionLogLogic')
 export default class UnionLogLogic extends Component {
     @property(ScrollView)
-    logView:ScrollView | null = null;
+    logView: ScrollView | null = null;
 
-    protected onLoad():void{
-        EventMgr.on(LogicEvent.unionLog,this.updateLog,this);
+    protected onLoad(): void {
+        EventMgr.on(LogicEvent.unionLog, this.updateLog, this);
     }
 
-    private applyLayout(): void {
-        const heading = ensureChild(this.node, '__UnionLogHeading');
-        heading.setPosition(0, 230, 0);
-        ensureTransform(heading, 1040, 54);
-        const graphics = heading.getComponent(Graphics) || heading.addComponent(Graphics);
-        graphics.clear();
-        graphics.fillColor = new Color(28, 23, 18, 236);
-        graphics.roundRect(-520, -27, 1040, 54, 9);
-        graphics.fill();
-        graphics.strokeColor = new Color(139, 96, 48, 195);
-        graphics.lineWidth = 1.5;
-        graphics.roundRect(-520, -27, 1040, 54, 9);
-        graphics.stroke();
-
-        const title = createGameText(
-            heading,
-            '__UnionLogTitle',
-            'NHẬT KÝ LIÊN MINH',
-            20,
-            GameTheme.colors.gold300,
-            360,
-            40,
-            true,
-        );
-        title.node.setPosition(-320, 0, 0);
-
-        const hint = createGameText(
-            heading,
-            '__UnionLogHint',
-            'Các hoạt động quản trị và thay đổi thành viên',
-            14,
-            GameTheme.colors.muted,
-            430,
-            34,
-        );
-        hint.node.setPosition(245, 0, 0);
-
-        if (!this.logView) {
-            return;
-        }
-        this.logView.node.setPosition(0, -32, 0);
-        ensureTransform(this.logView.node, 1040, 455);
-        const view = this.logView.node.getChildByName('view');
-        if (view) {
-            ensureTransform(view, 1040, 455);
-        }
-        if (this.logView.content) {
-            const transform = this.logView.content.getComponent(UITransform) || this.logView.content.addComponent(UITransform);
-            transform.width = 1040;
-        }
-        const list = this.logView.node.getComponent(ListLogic) as any;
-        if (list) {
-            list.columnCount = 1;
-            list.autoColumnCount = false;
-            list.isHorizontal = false;
-            list.spaceRow = 9;
-        }
-    }
-
-    protected onDestroy():void{
+    protected onDestroy(): void {
         EventMgr.targetOff(this);
     }
 
-    protected updateLog(data:any[]){
-        var comp = this.logView.node.getComponent(ListLogic);
-        comp.setData(data?data:[]);
+    protected updateLog(data: any[]): void {
+        if (!this.logView) {
+            return;
+        }
+        const comp = this.logView.node.getComponent(ListLogic);
+        if (comp) {
+            comp.setData(data || []);
+        }
     }
 
-    protected getLog():void{
-        let city:MapCityData = MapCommand.getInstance().cityProxy.getMyMainCity();
-        let unionData:Union = UnionCommand.getInstance().proxy.getUnion(city.unionId);
-        if(unionData && unionData.isMajor(city.rid)){
+    protected getLog(): void {
+        const city: MapCityData = MapCommand.getInstance().cityProxy.getMyMainCity();
+        const unionData: Union = UnionCommand.getInstance().proxy.getUnion(city.unionId);
+        if (unionData && unionData.isMajor(city.rid)) {
             UnionCommand.getInstance().unionLog();
         }
     }
 
-    protected onEnable():void{
-        this.applyLayout();
+    protected onEnable(): void {
         this.getLog();
     }
 }
